58e660d56bb38c41f53a2973ae6efba29ec99af0
xiaoli <xiaoli.com>
2022-11-26 09:30:56 +0800

    first commit

---
+++ untitled/src/main/java/com/li/GitTest.java
@@ -0,0 +1,12 @@
+package com.li;
+
+/**
+ * @author xiaoli
+ * @Date 2022-11-26-9:15
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+
+}
