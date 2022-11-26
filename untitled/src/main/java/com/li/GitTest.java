aa9a7ea1ee17a2014611fde3878f1e15fc7bde91
xiaoli <xiaoli.com>
2022-11-26 09:57:23 +0800

    second commit

--- untitled/src/main/java/com/li/GitTest.java
+++ untitled/src/main/java/com/li/GitTest.java
@@ -5,8 +5,9 @@ package com.li;
  * @Date 2022-11-26-9:15
  */
 public class GitTest {
+
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
-
 }
